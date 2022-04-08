d3af9eacf720e3878002ef648b2e21ae28803c6d
zolanalmodo <zoltanalmodo@mail.com>
2022-04-08 21:28:52 +0200

    First Commit

---
+++ src/context/BlogContext.js
@@ -0,0 +1,25 @@
+import createDataContext from './createDataContext';
+
+
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'addBlogPost':
+            return [...state, { title: `Blog Post #${state.length + 1}` }];
+        default:
+            return state;
+    }
+};
+
+const addBlogPost = (dispatch) => {
+
+    return () => {
+        dispatch({ type: 'addBlogPost' })
+    };
+    
+};
+
+export const { Context, Provider } = createDataContext(
+    blogReducer,
+    { addBlogPost },
+    [],
+);
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { Context as BlogContext } from '../context/BlogContext';
+
+const IndexScreen = () => {
+
+    const { state, addBlogPost } = useContext(BlogContext);
+
+    return (
+        
+        <View>
+            <Text>Index Screen</Text>
+            <Button
+                title='Add Blog Post'
+                onPress={addBlogPost}
+            />
+            <FlatList
+                data={state}
+                keyExtractor={(item) => item.title}
+                renderItem={({ item }) => {
+                    return <Text>{item.title}</Text>
+                }}
+            />
+        </View>
+    )
+};
+
+const styles = StyleSheet.create ({
+    header_text: {
+        color: 'coral',
+        fontSize: 22,
+        alignSelf: 'center'
+    },
+})
+
+export default IndexScreen;
